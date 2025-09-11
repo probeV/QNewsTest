53f15bd37fa4d6ac5b42bec39d8a2e9da7d09fd7
Ahnjaemin1208 <michael1208@naver.com>
2025-09-11 20:36:47 +0900

    feat : websocketconfig
    /ws 로 웹 소켓 연결 후, /ws/news로 뉴스들을 받아옴

---
+++ news/src/main/java/com/qraft/news/domain/news/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.qraft.news.domain.news.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Slf4j
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/ws/news");
+    }
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
